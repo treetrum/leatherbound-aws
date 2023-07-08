727486ce37032effd489398d85aa439edc8ae227
Sam Davis <sam@sjd.co>
2023-07-08 14:02:10 +1000

    Initial commit

---
+++ test/leatherbound-aws.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LeatherboundAws from '../lib/leatherbound-aws-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/leatherbound-aws-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LeatherboundAws.LeatherboundAwsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
